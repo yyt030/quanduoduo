89c3add548e6a466a8956b5c8c88e8ebd71a87cb
Yantao <yyt030@gmail.com>
2015-12-07 17:06:07 +0800

    券包查询

--- weshop/controllers/site.py
+++ weshop/controllers/site.py
@@ -246,9 +246,18 @@ def user_home():
 def tickets():
     """券包"""
     type = request.args.get("type", "not_use")
+    user_id = session['user_id']
     nav = 2
-    discounts = Discount.query.all()
-    return render_template('mobile/my_tickets.html', type=type, nav=2, discounts=discounts)
+    if type == 'not_use':
+        records = GetTicketRecord.query.filter(GetTicketRecord.user_id == user_id,
+                                               GetTicketRecord.status == 'normal')
+    elif type == 'expire':
+        records = GetTicketRecord.query.filter(GetTicketRecord.user_id == user_id,
+                                               GetTicketRecord.status == type)
+    else:
+        records = GetTicketRecord.query.filter(GetTicketRecord.user_id == user_id)
+        
+    return render_template('mobile/my_tickets.html', type=type, nav=2, records=records)
 
 
 @bp.route('/gonglue')
